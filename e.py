b3e539cb20e2f0829428dfe45a2b46375660421e
Mohammad Abdul Ahad <maahad.767@gmail.com>
2021-02-06 15:00:36 +0600

    added solution for E

--- e.py
+++ e.py
@@ -1,5 +1,9 @@
 def main():
-    
+    sentence = input().replace(".", '').replace(',', '').replace('!', '').replace('?', '').replace(' ', '')
+
+    while sentence != 'DONE':
+        print("You won't be eaten!" if sentence.lower() == sentence[::-1].lower() else "Uh oh..")
+        sentence = input().replace(".", '').replace(',', '').replace('!', '').replace('?', '').replace(' ', '')
 
 if __name__ == '__main__':
     main()
---
+++ f.py
@@ -0,0 +1,13 @@
+def main():
+    length = int(input())
+    numbers = map(lambda n: int(n), input().split())
+
+    i, c = 1, 0
+    while i < length - 1:
+        c += 1 if numbers[i-1] < numbers[i] < numbers[i+1] else 0
+
+    print(c)
+    
+
+if __name__ == '__main__':
+    main()
