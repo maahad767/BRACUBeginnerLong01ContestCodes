fb71b43f466eb09f4b8064396ef8e72f3afd1845
Mohammad Abdul Ahad <maahad.767@gmail.com>
2021-02-06 15:06:35 +0600

    added solution for F

--- f.py
+++ f.py
@@ -1,13 +1,14 @@
 def main():
     length = int(input())
-    numbers = map(lambda n: int(n), input().split())
+    numbers = list(map(lambda n: int(n), input().split()))
 
     i, c = 1, 0
     while i < length - 1:
-        c += 1 if numbers[i-1] < numbers[i] < numbers[i+1] else 0
+        c += 1 if numbers[i-1] < numbers[i] < numbers[i+1] or numbers[i-1] > numbers[i] > numbers[i+1]  else 0
+        i += 1
 
     print(c)
-    
+
 
 if __name__ == '__main__':
     main()
